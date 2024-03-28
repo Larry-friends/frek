ce0376624e449e8031734085f3ee58e15ceee047
LarryZhu-dev <995231030@qq.com>
2024-03-28 14:16:38 +0800

    fix: 移除灭霸

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import thanos from './scrips/thanos.js';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    {
-      name: 'thanos',
-      configureServer(server) {
-        thanos();
-      },
-    }
   ],
 })
